11ab25324f43634041b6ca9457942abfa5970a71
cdidriche <corentin.didriche@gmail.com>
2019-04-26 13:30:40 +0200

    Started integrating auto rotation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gcaesthetics-implantbox
 A project that scans a serial number on a breast implant
+
+
+## Interesting entry points
+
+main.py : complete app with detection until text
+
+eurosilicone-reader.py : acquisition app (saves chips to /var/eurosilicone/acqusitions)
+
+uploader.py : will upload acquisitions to Azure (use this in a CRON)
+
--- eurosilicone-reader.py
+++ eurosilicone-reader.py
@@ -31,6 +31,7 @@ import time
 import os
 import logging
 
+import numpy as np
 import camera
 import detection_instance
 import image
@@ -51,6 +52,106 @@ from settings import *
 class EurosiliconeReader(object):
     """Singleton class
     """
+
+    def process_chip(self, img_chip):
+        """Process chip
+        """
+        #Opencv to PILLOW image
+        img_chip_pil = Image.fromarray(img_chip)
+        img_chip_pil_rotate = img_chip_pil.copy()
+
+        #init array results
+        best_scores = np.array([0,0,0], dtype=float)
+        best_boxes = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+        best_deg = np.array([0,0,0], dtype=int)
+
+        #check every image rotate, with 10 deg step
+        for deg in range(0, 360, 10):
+            #inference function
+            is_detected, out_boxes, out_scores, out_classes = self.text_detection.detect_image(img_chip_pil_rotate)
+            #Check the best score detection for each text
+            if len(out_scores) == 3:
+                for i in range(len(out_scores)):
+                    if out_scores[i] > best_scores[i]:
+                        best_scores[i] = out_scores[i]
+                        best_deg[i] = deg
+
+                        for y in range(len(out_boxes[i])):
+                            best_boxes[i][y] = out_boxes[i][y]
+
+                #rotate image before saving to visualize... (For dev)
+                img_chip_pil_rotate = img_chip_pil.rotate(deg)
+                open_cv_image = np.array(img_chip_pil_rotate) 
+                #computeResults.saveImage(open_cv_image)
+
+            else :
+                continue
+
+        if len(best_scores) == 3:
+            #Crop texts detection
+            img_text1, img_text2, img_text3 = self.detect.get_text_area(best_boxes, best_deg[0], best_deg[1], best_deg[2])
+
+            #Save texte images
+            #computeResults.saveImage(img_text1)
+            #computeResults.saveImage(img_text2)
+            #computeResults.saveImage(img_text3)
+
+            #Convert to PIL format
+            img_text1_pil = Image.fromarray(img_text1)
+            img_text2_pil = Image.fromarray(img_text2)
+            img_text3_pil = Image.fromarray(img_text3)
+
+            #############################################PART 3##########################################
+            #Char detection
+            #Init list of char
+            list_line1 = []
+            list_line2 = []
+            list_line3 = []
+
+            #inference function
+            is_detected_t1, out_boxes_t1, out_scores_t1, out_classes_t1 = self.char_detection.detect_image(img_text1_pil)
+            is_detected_t2, out_boxes_t2, out_scores_t2, out_classes_t2 = self.char_detection.detect_image(img_text2_pil)
+            is_detected_t3, out_boxes_t3, out_scores_t3, out_classes_t3 = self.char_detection.detect_image(img_text3_pil)
+
+            #if at least 1 detection by line...
+            if (is_detected_t1 == True and is_detected_t2 == True and is_detected_t3 == True):
+                for char in out_classes_t1:
+                    list_line1.append(char)
+                for char in out_classes_t2:
+                    list_line2.append(char)
+                for char in out_classes_t3:
+                    list_line3.append(char)
+
+                #convert from list to concatenated string
+                list_line1_str = ''.join(map(str, list_line1))
+                list_line2_str = ''.join(map(str, list_line2))
+                list_line3_str = ''.join(map(str, list_line3))
+
+                final_list = [list_line1_str, list_line2_str, list_line3_str]
+
+                #Instructions for sending to Arduino and simulate keystrokes of a keyboard...
+                #Send string by string
+                self.keyboard.send(list_line1_str)
+                self.keyboard.send("  ")
+                self.keyboard.send(list_line2_str)
+                self.keyboard.send("  ")
+                self.keyboard.send(list_line3_str)
+
+                #add Serial Number on the final picture
+                #final_img = computeResults.addSerialNumber(final_list)
+                logging.info("Serial Number written")
+                #cam.saveImage(fullimg, img)
+                #computeResults.saveImage(final_img)
+                logging.info("Image saved")
+
+            else :
+                print ("Unable to find all char. Please move the prosthesis")
+                logging.info("All char not found")
+        else :
+            print ("Unable to find all text area. Please move the prosthesis")
+            logging.info("All texts not found")
+            
+
     def main(self,):
         """Main program loop.
         """
@@ -78,32 +179,42 @@ class EurosiliconeReader(object):
             img_pil = Image.fromarray(fullimg)
             # computeResults = image.Image(fullimg)
 
-            # Pastille detection, save image on-the-fly
+            # Pastille detection, save image on-the-fly.
+            # The goal here is to perform a FIRST detection, wait for 1s and perform a SECOND detection
+            # in order to avoid keeping/getting blurry images.
             is_detected, out_boxes, out_scores, out_classes = self.past_detection.detect_image(img_pil)
-            
-            # Text Detection if pastille is detected
-            if is_detected:
-                # Get detected zone
-                detect = detection_instance.DetectionInstance(fullimg)
-                is_cropped, img_chip = detect.get_chip_area(out_boxes)
+            if not is_detected:
+                print("Unable to find circle. Move the object.")
+                continue
 
-                # Save chip image
-                output_fn = os.path.join(ACQUISITIONS_PATH, "CHIP-{}.png".format(time.strftime("%Y-%m-%d-%H%M%S")))
-                Image.fromarray(img_chip).save(output_fn)
-                print ("Image saved")
-                #img_chip.save(output_fn)
+            print("DON'T MOVE!")
+            time.sleep(1)
+            fullimg, img = self.cam.grabbingImage()
+            is_detected, out_boxes, out_scores, out_classes = self.past_detection.detect_image(img_pil)
 
-                # computeResults.saveImage(img_chip)
-                print ("Image saved. Change/Turn prothesis. Waiting 5s before detecting again.")
-                time.sleep(5)
-                logging.info("Circle detected")
+            if not is_detected:
+                print("You moved...")
+                continue
+        
+            # Get detected zone
+            self.detect = detection_instance.DetectionInstance(fullimg)
+            is_cropped, img_chip = self.detect.get_chip_area(out_boxes)
 
-            else:
-                print ("Unable to find circle. Please move the prosthesis")
-                logging.info("Circle not found")
-                #with tempfile.NamedTemporaryFile(suffix=".png") as fp:
-                #    img_pil.save(fp.name)
-                #    os.system("img2txt -f ansi -W 100 {}".format(fp.name))
+            # Save chip image
+            output_fn = os.path.join(ACQUISITIONS_PATH, "CHIP-{}.png".format(time.strftime("%Y-%m-%d-%H%M%S")))
+            Image.fromarray(img_chip).save(output_fn)
+            output_fn = output_fn.replace("CHIP", "FULL")
+            Image.fromarray(fullimg).save(output_fn)
+            print ("Image saved: {}".format(output_fn))
+            #img_chip.save(output_fn)
+
+            # Get additional info
+            #self.process_chip(img_chip)
+
+            # computeResults.saveImage(img_chip)
+            print ("Image saved. Change/Turn prothesis. Waiting 5s before detecting again.")
+            time.sleep(5)
+            logging.info("Circle detected")
 
             # self.ui.displayImage(img)
 
