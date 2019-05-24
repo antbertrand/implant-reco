da2693ce61d5209096cbb8cec5a374423a0f25c1
abert1 <abertrand@numericube.com>
2019-05-24 13:42:52 +0200

    Cleaned code

--- step3_angle_correction/src/create_dataset.py
+++ step3_angle_correction/src/create_dataset.py
@@ -1,60 +1,41 @@
-import json
+"""Script that will create the dataset with the 360 rotations of each image.
+In this version has to be done for train, test and validation by changing paths. """
+
+
 import os
-from PIL import Image
-import cv2 as cv2
-import numpy as np
+
+import cv2
 import imutils
-import random
 
 
-#Script the will create the dataset with the 360 rotations of each image
 
-IMAGES_PATH = '/storage/eurosilicone/img_corr_resized/'
-OUTPUT_PATH = '/storage/eurosilicone/ds_rotated/'
-images = os.listdir(IMAGES_PATH)
 
-for index, im in enumerate(images):
+
+IMAGES_PATH = "/storage/eurosilicone/ds_corr_resized/img_train/img_corr/"
+OUTPUT_PATH = "/storage/eurosilicone/ds_rotated/train/"
+IMAGES = os.listdir(IMAGES_PATH)
+
+for index, im in enumerate(IMAGES):
 
     print(im)
-    img = cv2.imread(IMAGES_PATH + im)
-    img2 = cv2.resize(img, (224, 224))
+    img2 = cv2.imread(IMAGES_PATH + im, 0)
 
-    # Create test / train / valdiation directories
-    dir_train = OUTPUT_PATH + 'train/'
-    dir_test = OUTPUT_PATH + 'test/'
-    dir_val = OUTPUT_PATH + 'val/'
-    if not os.path.exists(dir_train):
-            os.makedirs(dir_train)
-    if not os.path.exists(dir_test):
-            os.makedirs(dir_test)
-    if not os.path.exists(dir_val):
-            os.makedirs(dir_val)
+    for k in range(0, 360):
+        # Creating rotation of the image by k degree
+        img3 = imutils.rotate(img2, k)
 
-    ndir = index%8
-
-
-    if ndir >=0 and ndir <6:
-        dir_final = dir_train
-    if ndir ==6:
-        dir_final = dir_val
-    if ndir ==7:
-        dir_final = dir_test
-
-
-    for k in range(0,360):
-
-        img3 = imutils.rotate(img2,k)
-
+        # k_3 is k written on 3 digits: 3 => 003
         k_3 = "{:03d}".format(k)
-        output_dir = dir_final + str(k_3) + '/'
-        print(output_dir)
 
+        # Creating folder k_3 if doesn't already exist
+        output_dir = OUTPUT_PATH + str(k_3) + "/"
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        cv2.imwrite( output_dir+im, img3 )
+
+        # Saves image in folder k_3
+        cv2.imwrite(output_dir + im, img3)
+
+    print("im", index, "done")
 
 
-    print('im', index, 'done')
-
-
-    print('finito')
+print("finito")
--- step3_angle_correction/src/train_angle_resnet.py
+++ step3_angle_correction/src/train_angle_resnet.py
@@ -1,73 +1,82 @@
+"""Script that will train a RotNet on our dataset. Paths are made to be used on Paperspace"""
+
+
 import os
-import random
+from time import time
 
-import keras
-from keras_preprocessing.image import ImageDataGenerator
-from keras.applications.resnet50 import ResNet50, preprocess_input
-
-import numpy as np
 
 # Import all the Keras machinery we need
-from keras import applications
-from keras import optimizers
-from keras.preprocessing import image
-from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D
+
 from keras import backend as K
-from keras import metrics
+from keras_preprocessing.image import ImageDataGenerator
+from keras.applications.resnet50 import ResNet50
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.layers import Dense, Flatten
+from keras.callbacks import (
+    ModelCheckpoint,
+    EarlyStopping,
+    TensorBoard,
+    ReduceLROnPlateau,
+)
 
 
-
-
-DATA_DIR = '/storage/eurosilicone/ds_rotated/'
+DATA_DIR = "/storage/eurosilicone/ds_rotated/"
 
 
 HEIGHT = 224
 WIDTH = 224
-batch_size = 4
+BATCH_SIZE = 32
 
 
 # data generator for training set
 train_datagen = ImageDataGenerator(
-    rescale = 1./255)
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rescale=1 / 255
+)
 
 # data generator for test set
-test_datagen = ImageDataGenerator(rescale = 1./255)
-
+test_datagen = train_datagen
 # generator for reading train data from folder
 train_generator = train_datagen.flow_from_directory(
-    DATA_DIR+'train',
-    target_size = (224, 224),
-    color_mode = 'rgb',
-    batch_size = batch_size,
-    class_mode = 'categorical')
+    DATA_DIR + "train",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+)
 
 # generator for reading validation data from folder
 validation_generator = test_datagen.flow_from_directory(
-    DATA_DIR+'val',
-    target_size = (224, 224),
-    color_mode = 'rgb',
-    batch_size = batch_size,
-    class_mode = 'categorical')
+    DATA_DIR + "val",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=BATCH_SIZE,
+    class_mode="categorical",
+)
 
 # generator for reading test data from folder
 test_generator = test_datagen.flow_from_directory(
-    DATA_DIR+'test',
-    target_size = (224, 224),
-    color_mode = 'rgb',
-    batch_size = 1,
-    class_mode = 'categorical',
-    shuffle = False)
+    DATA_DIR + "test",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=1,
+    class_mode="categorical",
+    shuffle=False,
+)
 
 
-#================== Angle_error metric =========================
+# ================== Angle_error metric =========================
 # Functions to compute the angle_error metric
+
 
 def angle_difference(x, y):
     """
     Calculate minimum difference between two angles.
     """
     return 180 - abs(abs(x - y) - 180)
+
 
 def angle_error(y_true, y_pred):
     """
@@ -80,17 +89,13 @@ def angle_error(y_true, y_pred):
     return K.mean(K.cast(K.abs(diff), K.floatx()))
 
 
-
-#================== Model creation =========================
+# ================== Model creation =========================
 # Model = Resnet50 without top classifier / without freezing layers
 
-from keras.optimizers import Adam
-from keras.layers import Dense, Dropout, Flatten, Input
-from keras.layers import Conv2D, MaxPooling2D
-from keras.models import Model
 
 
-model_name = 'rotnet_chip_resnet50'
+
+model_name = "rotnet_chip_resnet50_v2"
 
 # number of classes
 nb_classes = 360
@@ -98,66 +103,65 @@ nb_classes = 360
 input_shape = (224, 224, 3)
 
 # load base model
-base_model = ResNet50(weights='imagenet', include_top=False,
-                  input_shape=input_shape)
+BASE_MODEL = ResNet50(weights="imagenet", include_top=False, input_shape=input_shape)
 
 # append classification layer
-x = base_model.output
-x = Flatten()(x)
-final_output = Dense(nb_classes, activation='softmax', name='fc360')(x)
+X = BASE_MODEL.output
+X = Flatten()(X)
+final_output = Dense(nb_classes, activation="softmax", name="fc360")(X)
 
 # create the new model
-model = Model(inputs=base_model.input, outputs=final_output)
+model = Model(inputs=BASE_MODEL.input, outputs=final_output)
 
 model.summary()
 
 # model compilation
-model.compile(loss='categorical_crossentropy',
-          optimizer=Adam(lr = 1e-5),
-          metrics=[angle_error])
+model.compile(
+    loss="categorical_crossentropy", optimizer=Adam(lr=1e-5), metrics=[angle_error]
+)
+
+
+# ================== Training model =========================
 
 
 
-#================== Training model =========================
-
-from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, ReduceLROnPlateau
-from time import time
 
 
 # training parameters
-batch_size = 32
+BATCH_SIZE = 32
 nb_epoch = 30
-n_train_samples = len(train_generator) * batch_size
-n_val_samples = len(validation_generator) * batch_size
+n_train_samples = len(train_generator) * BATCH_SIZE
+n_val_samples = len(validation_generator) * BATCH_SIZE
 
 
-output_folder = '/artifacts/models/'
+output_folder = "/artifacts/models/"
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 # Callback functions
 
-monitor = 'val_angle_error'
+monitor = "val_angle_error"
 
 # Always saving the model with the best val_angle_error
 checkpointer = ModelCheckpoint(
-    filepath=os.path.join(output_folder, model_name + '.hdf5'),
+    filepath=os.path.join(output_folder, model_name + ".hdf5"),
     monitor=monitor,
-    verbose = 2,
-    mode = 'min',
-    save_best_only=True
+    verbose=2,
+    mode="min",
+    save_best_only=True,
 )
-reduce_lr = ReduceLROnPlateau(monitor=monitor, factor = 0.3, patience=3)
+reduce_lr = ReduceLROnPlateau(monitor=monitor, factor=0.3, patience=3)
 early_stopping = EarlyStopping(monitor=monitor, patience=5)
-tensorboard = TensorBoard(log_dir='/artifacts/logs/{}'.format(time()))
+tensorboard = TensorBoard(log_dir="/artifacts/logs/{}".format(time()))
 
 
 history = model.fit_generator(
     train_generator,
-    steps_per_epoch=n_train_samples // batch_size,
+    steps_per_epoch=n_train_samples // BATCH_SIZE,
     epochs=nb_epoch,
     validation_data=validation_generator,
-    validation_steps=n_val_samples // batch_size,
-    verbose = 2,
-    callbacks=[checkpointer, tensorboard])
+    validation_steps=n_val_samples // BATCH_SIZE,
+    verbose=2,
+    callbacks=[checkpointer, tensorboard],
+)
