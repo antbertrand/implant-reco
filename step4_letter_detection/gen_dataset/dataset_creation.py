3445481e4610af01ecb5436c600740aa0ca5d27e
HD <heldelcourt@HD-Machine.local>
2019-05-29 15:50:40 +0200

    Small optim on chip generation

--- step4_letter_detection/gen_dataset/dataset_creation.py
+++ step4_letter_detection/gen_dataset/dataset_creation.py
@@ -11,17 +11,20 @@ from gen_data import generate_chip
 
 
 
-def create_chips(dest_folder="."):
+def create_chips(dest_folder=".", side_length=224):
 
     nb_chips = 3
     data = []
 
     for k in range(nb_chips):
 
-          chip, coords, caracs, letters_size = generate_chip()
+          chip, boxes, caracs = generate_chip()
           path_name = os.path.join(dest_folder, "GENERATED_CHIP-{}.jpg".format(k))
+          scale = chip.shape[0] / side_length
+          chip = cv2.resize(chip, (side_length, side_length))
+          boxes *= scale
           cv2.imwrite(path_name, chip)
-          data.append({"path": os.path.abspath(path_name), "coords": coords, "caracs": caracs, "carac_size": letters_size})
+          data.append({"path": os.path.abspath(path_name), "coords": boxes, "caracs": caracs})
           write_csv(data)
 
 
@@ -40,8 +43,8 @@ def write_csv(data, file_name = "ann_all.csv"):
                 coord = "{},{},{},{}".format(
                     datum['coords'][i][0],
                     datum['coords'][i][1],
-                    datum['coords'][i][0]+datum['carac_size'][0],
-                    datum['coords'][i][1]+datum['carac_size'][1],
+                    datum['coords'][i][2],
+                    datum['coords'][i][3],
                 )
                 f.write(",".join([datum['path'], coord, datum["caracs"][i]]))
                 f.write("\n")
