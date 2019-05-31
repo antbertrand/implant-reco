5fca44783b63d19c819974b53de524ea606dfadb
abert1 <abertrand@numericube.com>
2019-05-31 12:44:59 +0200

    Added paperspace training files step4

---
+++ step4_letter_detection/paperspace/dataset_creation.py
@@ -0,0 +1,80 @@
+
+
+import os
+import numpy as np
+import cv2
+
+from gen_data import generate_chip
+
+
+
+
+def create_chips(dest_folder=".", n=1000, side_length=800):
+    """ Create n artificial chips
+    """
+
+    for k in range(n):
+        data = []
+        # Generate a chip and set a path name
+        chip, boxes, caracs = generate_chip()
+        path_name = os.path.join(dest_folder, "GENERATED_CHIP-{}.jpg".format(k))
+
+        # Resize the chip and adapt the coordinates
+        scale = chip.shape[0] / side_length
+        chip = cv2.cvtColor(chip, cv2.COLOR_RGB2GRAY)
+        chip = cv2.equalizeHist(chip)
+        chip = cv2.resize(chip, (side_length, side_length))
+
+        boxes = np.array(boxes)
+        boxes = boxes.astype(float)
+        boxes /= scale
+
+
+        # Save the image
+        if k<n*0.8:
+            path_name = os.path.join(dest_folder, "train", "GENERATED_CHIP-{}.jpg".format(k))
+            file_name = "/storage/eurosilicone/ds_step4/split_dataset_big2/ann_train.csv"
+
+        else:
+            path_name = os.path.join(dest_folder, "test", "GENERATED_CHIP-{}.jpg".format(k))
+            file_name = "/storage/eurosilicone/ds_step4/split_dataset_big2/ann_test.csv"
+
+        cv2.imwrite(path_name, chip)
+        # Set all the data we need for annotation
+        data.append({"path": os.path.abspath(path_name), "coords": boxes.astype(int), "caracs": caracs})
+
+        # Write the annotation of this chip in the csv file
+        write_csv(data, file_name)
+
+
+
+
+
+
+def write_csv(data, file_name = "ann_all.csv"):
+    """ Write a csv file for annotations of chips
+    """
+    # If the file doesn't exist, create it
+    if not os.path.isfile(file_name):
+        with open(file_name, 'w'):
+            pass
+
+    # Then append, the annotations of the chip
+    with open(file_name, 'a') as f:
+        for datum in data:
+            for i in range(len(datum['coords'])):
+                coord = "{},{},{},{}".format(
+                    datum['coords'][i][0],
+                    datum['coords'][i][1],
+                    datum['coords'][i][2],
+                    datum['coords'][i][3],
+                )
+                # Write the path of the image, the coordinates and the type of each letter
+                f.write(",".join([datum['path'], coord, datum["caracs"][i]]))
+                f.write("\n")
+
+with open("/storage/eurosilicone/ds_step4/split_dataset_big2/ann_train.csv", 'w'):
+    pass
+with open("/storage/eurosilicone/ds_step4/split_dataset_big2/ann_test.csv", 'w'):
+    pass
+create_chips(dest_folder='/storage/eurosilicone/ds_step4/split_dataset_big2/')
---
+++ step4_letter_detection/paperspace/run.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+#
+# Install additional modules
+#apt-get install unzip
+
+pip install --upgrade pip
+
+#pip install zipfile36
+pip install progressbar2
+
+pip install keras
+pip install numpy
+pip install opencv-python
+pip install imutils
+pip install Pillow
+pip install 'cython'
+pip install 'keras-resnet'
+pip install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
+pip install 'h5py'
+pip install 'keras'
+pip install 'matplotlib'
+pip install 'numpy>=1.14'
+pip install 'opencv-python>=3.3.0'
+pip install 'pillow'
+pip install 'progressbar2'
+pip install 'pytest-flake8'
+pip install 'pytest-xdist'
+pip install 'tensorflow'
+pip uninstall tensorflow
+pip install tensorflow-gpu
+
+# Start tensorboard
+#mkdir -p /artifacts/tensorboard_logs/
+#tensorboard --logdir=/artifacts/tensorboard_logs/ --port=6006 &
+
+#Create useful folders
+# mkdir -p /artifacts/
+# mkdir /storage/test/
+#mkdir /storage/eurosilicone
+#mkdir /storage/eurosilicone/ds_rotated
+#mkdir /artifacts/models
+
+
+#mkdir /storage/eurosilicone/ds_step4
+#mkdir /storage/eurosilicone/ds_step4/split_dataset_big2
+#mkdir /storage/eurosilicone/ds_step4/split_dataset_big2/test
+#mkdir /storage/eurosilicone/ds_step4/split_dataset_big2/train
+#rm -r /storage/eurosilicone/ds_rotated
+#rm -r /storage/eurosilicone/img_corr_resized/
+#rm -r /storage/eurosilicone/ds_corr_resized/
+#mkdir /storage/eurosilicone/ds_rotated3
+#mkdir /storage/eurosilicone/ds_rotated3/train
+#mkdir /storage/eurosilicone/ds_rotated3/val
+#mkdir /storage/eurosilicone/ds_rotated3/test
+# mkdir /storage/test/masks/
+#
+#mv /paperspace/ds_corr_resized_uncropped /storage/eurosilicone/
+#unzip /storage/eurosilicone/ds_corr_full/val/img_corr_full.zip -d /storage/eurosilicone/ds_corr_full/val/
+#unzip /storage/eurosilicone/ds_corr_full/train/img_corr_full.zip -d /storage/eurosilicone/ds_corr_full/train/
+
+#cd /paperspace/keras_retinanet
+
+python3 test.py
+#python3 dataset_creation.py
+#python3 zip.py
+#python3 create_dataset.py
+#keras_retinanet/bin/train.py --batch-size=1 --random-transform --compute-val-loss --steps=200 --weighted-average csv ../gen_dataset/ann_train.csv ../gen_dataset/class_mapping.csv --val-annotations ../gen_dataset/ann_test.csv
+/paperspace/keras_retinanet/bin/debug.py --annotations csv /storage/eurosilicone/ds_step4/split_dataset_big2/ann_train.csv /storage/eurosilicone/ds_step4/split_dataset_big2/class_mapping.csv
+
+/paperspace/keras_retinanet/bin/train.py --batch-size=1 --lr=1e-4 --random-transform --compute-val-loss --steps=10 --weighted-average --snapshot-path /artifacts/models/ csv /storage/eurosilicone/ds_step4/split_dataset_big2/ann_train.csv /storage/eurosilicone/ds_step4/split_dataset_big2/class_mapping.csv --val-annotations /storage/eurosilicone/ds_step4/split_dataset_big2/ann_test.csv
